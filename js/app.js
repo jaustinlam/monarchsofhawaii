b2fb4e46bd38628e3bde280bf0b73c95c8e7555e
jaustinlam <j.a.lam@live.com>
2017-01-18 20:06:03 -0800

    Initial commit

--- js/app.js
+++ js/app.js
@@ -3,7 +3,10 @@ $(document).ready(function(){
 	
 });
 
-function initMap(){
+function init(){
+	var Model = function(){
+		
+	}
 	function MapViewModel(){
 		var self = this;
 		
@@ -27,7 +30,8 @@ function initMap(){
 		var fourSquareTimeout = setTimeout(function(){
 			alert("Failed to get restaurant data");
 		}, 8000);
-		// ajax request for FourSquare API
+
+		// AJAX request for FourSquare API
 		$.ajax({
 			url: 'https://api.foursquare.com/v2/venues/search?ll=' + lat + "," + long,
 			dataType: 'jsonp',
@@ -38,78 +42,89 @@ function initMap(){
 					"&query=noodles",
 					// TO DO SET TIME AND DATE TO NOW
 			success: function(data) {
+				// Success message
 				console.log("Data received");
+				// Clear the Timeout Error
 				clearTimeout(fourSquareTimeout);
 
+				// Empty array to store restaurants
 				var restaurants = [];
+				// Set returns to self.results
 				self.results = data.response.venues;
 
+				// Clear local store if data exists
 				if(localStorage.restaurants){
 					localStorage.clear();
 				};
 				for (var i = self.results.length - 1; i >= 0; i--) {
+					var currentRestaurant = self.results[i];
 					var restaurant = {
-						name: self.results[i].name
-					}
+						id: currentRestaurant.id,
+						name: currentRestaurant.name,
+						website: currentRestaurant.url,
+						lat: currentRestaurant.location.lat,
+						lng: currentRestaurant.location.lng,
+						visible: true,
+					};
+					// Push restaurant object to restaurants array
 					restaurants.push(restaurant);
-					
-					
-
-
 				};
-				localStorage.setItem("restaurants", JSON.stringify(restaurant));
+				// Save restaurants array to local storage
+				localStorage.setItem("restaurants", JSON.stringify(restaurants));
 				console.log(localStorage.restaurants);
-				
-
-
-
-				function toggleBounce(){
-					if (self.marker.getAnimation() !== null) {
-						self.marker.setAnimation(null);
-					} else {
-						self.marker.setAnimation(google.maps.Animation.BOUNCE);
-					}
-				};
-				
-				for (var i = self.results.length - 1; i >= 0; i--) {
-					self.contentString = 'This is a test';
-					self.infowindow = new google.maps.InfoWindow({
-						content: self.contentString
-					});
-					self.lng = self.results[i].location.lng;
-					self.lat = self.results[i].location.lat;
-					// Markers
-					var icon = 'images/82812-200.png';
-					// var selectedIcon 
-					self.marker = new google.maps.Marker({
-						position: {lat: self.lat, lng: self.lng },
-						map: self.map,
-						icon: icon,
-						animation: google.maps.Animation.DROP
-					});
-					// self.marker.addListener('click', toggleBounce);
-					self.marker.addListener('click', (function(markerCopy){
-						return function(){
-							self.infowindow.open(self.map, markerCopy);
-							if (markerCopy.getAnimation() !== null) {
-								markerCopy.setAnimation(null);
-							} else {
-							markerCopy.setAnimation(google.maps.Animation.BOUNCE);
-							};
-						};
-					})(self.marker));
-
-				};
-				console.log(self.results);
-				
+			
 			},
+			// Error if fails
 			error: function(){
 				alert("Sorry data failed to load")
 			} 
 		});
+		// Array of restaurants
+		this.mapRestaurants = ko.observableArray(JSON.parse(localStorage.restaurants));
 
+		// Placing Markers on Mapa
+		var markerArray = [];
+		for (var i = self.mapRestaurants().length - 1; i >= 0; i--) {
+			self.contentString = '<h6>' + self.mapRestaurants()[i].name + '</h6>';
+			self.lng = self.mapRestaurants()[i].lng;
+			self.lat = self.mapRestaurants()[i].lat;
+			// Markers
+			var icon = 'images/icon.png';
+			// var selectedIcon 
+			self.marker = new google.maps.Marker({
+				position: {lat: self.lat, lng: self.lng },
+				map: self.map,
+				icon: icon,
+				animation: google.maps.Animation.DROP,
+				id: self.mapRestaurants()[i].id
+			});
+			self.marker.info = new google.maps.InfoWindow({
+				content: self.mapRestaurants()[i].name
+			})
+			markerArray.push(self.marker);
 
+			// self.marker.addListener('click', toggleBounce);
+			self.marker.addListener('click', (function(markerCopy){
+				return function(){
+					markerCopy.info.open(self.map, markerCopy);
+					if (markerCopy.getAnimation() !== null) {
+						markerCopy.setAnimation(null);
+					} else {
+					markerCopy.setAnimation(google.maps.Animation.BOUNCE);
+					};
+				};
+			})(self.marker));
+		};
+
+		this.clickOpen = function(data, event){
+			console.log("you clicked" + event.target.id);
+			console.log(markerArray[0].id);
+
+		}
 	};
+
+	
+
 	ko.applyBindings(new MapViewModel());
 };
 
